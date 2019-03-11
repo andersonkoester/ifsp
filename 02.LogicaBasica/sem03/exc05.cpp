3ff8346ae67540885f75903640604bab10cdab54
Anderson Koester <anderson.koester@megaembalagens.com.br>
2019-03-11 08:51:35 -0300

    semana 3

---
+++ 02.LogicaBasica/sem03/exc05.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(int argc, char *argv[]){
+	int x = 0;
+	while ( x < 10 ){
+		printf("\nO valor de X eh %d", x);
+	}
+	system("PAUSE");
+	return 0;
+}
